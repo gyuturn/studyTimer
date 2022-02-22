c6b1de855198866b3b7ae4dd760824201c926f35
turn <kksy2423@naver.com>
2022-02-23 02:57:26 +0900

    로그인 후 화면 구현(~님까지 완료)

--- src/main/java/com/talk/randomTalk/controller/LoginController.java
+++ src/main/java/com/talk/randomTalk/controller/LoginController.java
@@ -9,9 +9,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequiredArgsConstructor
@@ -49,16 +54,32 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String loginAuthentication(@Validated @ModelAttribute("loginForm") LoginForm loginForm, BindingResult result) {
+    public String loginAuthentication(@Validated @ModelAttribute("loginForm") LoginForm loginForm,
+                                      BindingResult result,
+                                      HttpServletResponse response) {
         if (result.hasErrors()) {
             return "error";
         }
 
         //로그인 성공한 경우
         if (memberService.validLogin(loginForm)) {
-            System.out.println(loginForm.getId()+"님이 로그인되었습니다!");
-            return "redirect:/";
+
+            Cookie idCookie = new Cookie("memberId", String.valueOf(loginForm.getId()));
+            response.addCookie(idCookie);
+
+            return "redirect:/loginHome";
         }
         return "error";
+    }
+
+    @GetMapping("/loginHome")
+    public String homeLogin( HttpServletRequest request, Model model) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies.length != 1) {
+            return "redirect:/";
+        }
+        String memberId = cookies[0].getValue();
+        model.addAttribute("memberId", memberId);
+        return "member/loginHome";
     }
 }
