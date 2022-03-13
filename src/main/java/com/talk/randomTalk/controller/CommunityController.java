04611dc90982b70a5ecbd70257396c47543995ee
turn <kksy2423@naver.com>
2022-03-13 23:32:13 +0900

    커뮤니티 홈

--- src/main/java/com/talk/randomTalk/controller/CommunityController.java
+++ src/main/java/com/talk/randomTalk/controller/CommunityController.java
@@ -1,4 +1,20 @@
 package com.talk.randomTalk.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class CommunityController {
+
+    @GetMapping("community/home")
+    public String home(){
+
+        return "community/home";
+    }
+
+    @GetMapping("community/write")
+    public String write(){
+
+        return "community/write";
+    }
 }
